dd945d6976eb25d1a403520ab44cb1299bcab1cb
eugenio matias brave <eugenio_m_brave@hotmail.com>
2023-05-02 21:40:00 -0300

    mejoras del desfio 02/05

--- desafioECMAS.js
+++ desafioECMAS.js
@@ -1,27 +1,26 @@
 class ProductManager {
+   #products;
     constructor () {
-        this.products = [];
+        this.#products = []
     }
 
     getProducts() { 
-        console.log (this.products)
-        return this.products;
+        console.log (this.#products)
+        return this.#products;
     }
 
-    getProductById(id) {
-        const searching = this.products.find(prod => prod.id === id);
-          if (searching){
-             return searching;
-          } else {
-            return  console.log("Not found");  
-          }
-        } 
+    getProductById(id){
+        const searching = this.#products.find(prod => prod.id === id); 
+        searching??console.log("Not found");
+     }    
+
+        
 
     #createId ()
         { 
         let maxId= 0;  // con variable privada no estaria perdiendo el 0
-        for (let i=0 ; i < this.products.length; i++ ) { 
-        const prod = this.products [i];
+        for (let i=0 ; i < this.#products.length; i++ ) { 
+        const prod = this.#products [i];
 
         if (prod.id > maxId){ 
                 maxId=prod.id ; 
@@ -31,20 +30,20 @@ class ProductManager {
 
     addProduct( title ,description ,price ,thumbnail ,code ,stock) 
         { 
-        if (this.products.find(prod => prod.code === code)) {  // permite encontrar codigo repetido
-         return console.log( `The code ${code} is repeated`); 
+          if (this.#products.find(prod => prod.code === code)) {  // permite encontrar codigo repetido
+          return console.log( `The code ${code} is repeated`); 
              }
     
 
-     let newproducts= {id: this.#createId(), title,description, price,thumbnail, code ,stock };
+          let newproducts= {id: this.#createId(), title,description, price,thumbnail, code ,stock };
 
-     if (!Object.values(newproducts).includes(undefined)) {  // todos los campos son obligatorios tienen un valor, si no falta un valor todo OK, sino sera un valor UNDEFINED. 
+          if (!Object.values(newproducts).includes(undefined)) {  // todos los campos son obligatorios tienen un valor, si no falta un valor todo OK, sino sera un valor UNDEFINED. 
 
-     this.products = [...this.products, newproducts]; // .........uso de expredOperator
+         this.#products = [...this.#products, newproducts]; // .........uso de expredOperator
 
-     } else {  console.log("All fields are required")}
+        } else {  console.log("All fields are required")}
 
-     return  "addObjet"
+     return  newproducts;
 
     }          
 }
@@ -58,4 +57,5 @@ productM.addProduct("pats", "T. XL", 3000, "thumbnail 4", "abc126", 50 );
     
 
     console.log(productM.products); // testing...
-    productM.getProductById(2) // testing...
+    productM.getProductById(9) // testing...
+    
